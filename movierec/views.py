2ab8128c2b21515c1b683fa9a24a7bd171c529cd
KshitijMhatre <kshitijpmhatre@gmail.com>
2018-02-01 21:28:52 +0530

    login functionality added

--- movierec/views.py
+++ movierec/views.py
@@ -1,13 +1,16 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .forms import UserForm
+
 
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return render(request, 'movierec/preview.html')
     else:
-        return HttpResponse("Hello, world. You're at the polls index.")
+        return render(request, 'movierec/index.html')
 
 def login_user(request):
     if request.method == "POST":
@@ -16,8 +19,7 @@ def login_user(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
-                login(request, user)
-                albums = Album.objects.filter(user=request.user)
+                login(request, user)                
                 return render(request, 'movierec/index.html', {})
             else:
                 return render(request, 'movierec/login.html', {'error_message': 'Your account has been disabled'})
